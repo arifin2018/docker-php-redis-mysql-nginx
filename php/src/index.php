cd308190dbeb5ebfdab6aa95d20fea510c079649
arifinlenna <arifin@lenna.ai>
2022-08-07 20:32:39 +0700

    fixing redis nosql docker-compose connection

--- php/src/index.php
+++ php/src/index.php
@@ -1,19 +1,17 @@
 <?php
 
-// phpinfo();
-
 require_once dirname(__FILE__) . '/vendor/autoload.php';
 
 use Predis\Client;
 
 try {
-    $redis = new Client();
-
-    $redis->connect('redis', 6379);
-    // printf("Connection to redis has been established...\n");
-    echo "Connection to server sucessfully" . PHP_EOL;
-    //check whether server is running or not 
-    echo "Server is running: " . $redis->ping() . PHP_EOL;
+    $redis = new Client([
+        'host'      => 'redis',
+        'port'      => 6379,
+        'password'  => 'REDIS_PASSWORD'
+    ]);
+    $redis->set('name', 'Wai Yan Hein');
+    echo $redis->get('name') . PHP_EOL;
 } catch (Exception $e) {
-    die($e->getMessage() . PHP_EOL);
+    echo $e->getMessage() . PHP_EOL;
 }
