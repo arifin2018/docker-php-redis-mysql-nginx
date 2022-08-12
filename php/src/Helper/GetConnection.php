595b4183c2312626f4d05c807453e19439cd00bc
arifinlenna <arifin@lenna.ai>
2022-08-13 02:47:54 +0700

    all commit

---
+++ php/src/Helper/GetConnection.php
@@ -0,0 +1,20 @@
+<?php
+
+function GetConnection()
+{
+    $host = "database";
+    $port = 3306;
+    $database = "MYSQL_DATABASE";
+    $username = "root";
+    $password = "MYSQL_ROOT_PASSWORD";
+
+
+    return new PDO("mysql:host=$host;port=$port;dbname=$database", $username, $password);
+}
+// try {
+//     $conn = new PDO("mysql:host=$host;port=$port;dbname=$database", $username, $password);
+//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//     echo "Connected succesfully" . PHP_EOL;
+// } catch (PDOException $e) {
+//     echo "Connection failed: " . $e->getMessage() . PHP_EOL;
+// }
